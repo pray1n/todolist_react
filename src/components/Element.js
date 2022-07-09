b8254752ef808138814f95ad880704e72d478bea
Tilo Rabs <trabs@web.de>
2022-07-09 16:22:27 +0200

    update

--- src/components/Element.js
+++ src/components/Element.js
@@ -10,7 +10,8 @@ function Element({entry, updateEntry, handleEnter, deleteEntry}) {
   //or: onChange={({target}) => updateEntry(entry.id, target.value)}
   return (
     <div className="todoElement">
-      <input type="text" value={entry.text} onChange={(event) => updateEntry(entry.id, entry.complete, event.target.value)} />
+      <input type="text" value={entry.text} onChange={(event) => updateEntry(entry.id, entry.complete, event.target.value)} 
+        disabled={entry.complete} className={entry.complete?"linethrough":""} />
       <button onClick={() => deleteEntry(entry.id)}>Delete</button>
       <input type="checkbox" checked={entry.complete} onChange={(event) => updateEntry(entry.id, event.target.checked, entry.text)} />
     </div>
